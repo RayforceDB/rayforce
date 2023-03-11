03af3dff382da02b222ed479922bebd30f27b772
Anton <singaraiona@gmail.com>
2023-03-11 10:18:07 +0200

    lic

--- core/format.c
+++ core/format.c
@@ -1,3 +1,26 @@
+/*
+ *   Copyright (c) 2023 Anton Kundenko <singaraiona@gmail.com>
+ *   All rights reserved.
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,6 +33,14 @@
 #define F64_PRECISION 4
 
 const str_t PADDING = "                                                                                                   ";
+
+typedef struct padding_t
+{
+    u32_t left;
+    u32_t width;
+    u32_t height;
+
+} padding_t;
 
 extern str_t str_fmt(u32_t lim, str_t fmt, ...)
 {
