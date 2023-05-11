121917a3ed96fc2e6686860a3e3b2ba779f6740c
Anton <singaraiona@gmail.com>
2023-05-11 19:03:14 +0300

    fixed symbols

--- core/hash.c
+++ core/hash.c
@@ -41,6 +41,7 @@ hash_table_t *ht_new(i32_t size, i64_t (*hasher)(null_t *a), i32_t (*compare)(nu
                                            PROT_READ | PROT_WRITE,
                                            MAP_ANONYMOUS | MAP_PRIVATE,
                                            -1, 0);
+
     memset(buckets, 0, sizeof(bucket_t *) * size);
 
     table->cap = size;
